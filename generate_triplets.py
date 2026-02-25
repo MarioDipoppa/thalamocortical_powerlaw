cb62c986f026983ec3c9575b894902ddfd34a92f
Sakin Kirti <sakin.arya.kirti@gmail.com>
2026-02-25 11:18:48 -0800

    adding ringach model

---
+++ generate_triplets.py
@@ -0,0 +1,62 @@
+import argparse
+
+import numpy as np
+import imageio.v3 as iio
+from tqdm import tqdm
+
+def main(args:argparse.ArgumentParser):
+    
+    # for deterministic behavior
+    np.random.seed(args.seed)
+    
+    # load the entire video into memory
+    props = iio.improps(args.video)
+    video = iio.imread(args.video, index=None).astype(np.float32) / 255.
+    video = np.dot(video, [[0.2989], [0.5870], [0.1140]])   # convert to grayscale
+    
+    # compute the (x, y) top-left coords to use
+    X = np.random.uniform(0, props.shape[2] - int(args.crop_size), int(args.n_triplets) // props.n_images)
+    Y = np.random.uniform(0, props.shape[1] - int(args.crop_size), int(args.n_triplets) // props.n_images)
+    
+    # create a meshgrid to vectorize the crop generation
+    offsets = np.arange(int(args.crop_size))
+    dx, dy = np.meshgrid(offsets, offsets, indexing='xy')
+    
+    # store the set of x and y pixels per crop
+    X = (X[:, None, None] + dx[None, :, :]).astype(np.uint32)
+    Y = (Y[:, None, None] + dy[None, :, :]).astype(np.uint32)
+    
+    # set up the storage on disk
+    triplets = np.lib.format.open_memmap(args.out, mode="w+", dtype=np.float32, shape=(int(args.n_triplets), 3, int(args.crop_size), int(args.crop_size)))
+    
+    for i,f in tqdm(enumerate(video)):
+        
+        # generate the triplets
+        a = f[Y, X].squeeze()
+        if i + int(args.an_dist) > props.n_images - 1:
+            p = video[i-int(args.ap_dist)][Y, X].squeeze()
+            n = video[i-int(args.an_dist)][Y, X].squeeze()
+        else:
+            p = video[i+int(args.ap_dist)][Y, X].squeeze()
+            n = video[i+int(args.an_dist)][Y, X].squeeze()
+            
+        # store the triplets
+        triplet_set = np.stack([a, p, n], axis=1)
+        for j,k in zip(range(i * (int(args.n_triplets) // props.n_images)), range(int(args.n_triplets) // props.n_images)):
+            triplets[i * (int(args.n_triplets) // props.n_images) + j] = triplet_set[k]
+
+if __name__ == "__main__":
+    
+    # pase the arguments the user gives and just pass them to main
+    parser = argparse.ArgumentParser(description="generate triplets from a given video")
+    parser.add_argument("--video", help="path to video to generate triplets from")
+    parser.add_argument("--ap-dist", help="the number of frames that differentiate the anchor and positive images", default=1)
+    parser.add_argument("--an-dist", help="the number of frames that differentiate the anchor and negative images", default=50)
+    parser.add_argument("--n-triplets", help="the total number of triplets to generate", default=10000)
+    parser.add_argument("--crop-size", help="the dimensions of each of the images in the triplet", default=224)
+    parser.add_argument("--data-key", help="the key in the .h5 file where the data is stored under")
+    parser.add_argument("--out", help=".npy filepath where triplets should be stored")
+    parser.add_argument("--seed", help="random seed for consistency", default=1234)
+    args = parser.parse_args()
+    
+    main(args)
--- model_feedforward.py
+++ model_feedforward.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import numpy as np
 
 import torch
@@ -7,7 +8,9 @@ import torch.nn.functional as F
 class feedforward_VVS(nn.Module):
 
     def __init__(self, input_shape,
-        lgn_kernel_size=3, lgn_sx1:int=0.5, lgn_sy1:int=0.5, lgn_sx2:int=1, lgn_sy2:int=1
+        lgn_kernel_size=3, lgn_kernel_stride=2, lgn_sx1:int=0.5, lgn_sy1:int=0.5, lgn_sx2:int=1, lgn_sy2:int=1,
+        v1_kernel_size=3, v1_kernel_stride=2,
+        activation:Literal["relu", "sigmoid", "none"]="sigmoid"
         ):
         super().__init__()
 
@@ -25,17 +28,33 @@ class feedforward_VVS(nn.Module):
         # the off center cells are just the negative kernel
         off_lgn_kernel = -on_lgn_kernel
 
-        # stack and set up the kernels
-        self.lgn_kernel = torch.stack((on_lgn_kernel, off_lgn_kernel), dim=0).unsqueeze(1)
+        # stack and set up the lgn kernels
+        self.register_buffer("lgn_kernel", torch.stack((on_lgn_kernel, off_lgn_kernel), dim=0).unsqueeze(1))
+        self.lgn_stride = lgn_kernel_stride
 
         # create our v1 layer
-        self.v1 = nn.Conv2d(self.C * 2, self.C*2 * 8, kernel_size=3, stride=2, bias=False, device=self.device)
-        self.sigmoid = nn.Sigmoid()
+        self.v1 = nn.Conv2d(self.C * 2, self.C * 8, kernel_size=v1_kernel_size, stride=v1_kernel_stride, bias=False, device=self.device)
+        
+        # set up the activation
+        match activation:
+            case "none":
+                self.activation = None
+            case "sigmoid":
+                self.activation = nn.Sigmoid()
+            case "relu":
+                self.activation = nn.ReLU()
 
-    def forward(self, x):
+    def forward(self, x, rep:Literal["all", "final"]="final"):
 
         # go through our layers
-        lgn = self.sigmoid( F.conv2d(x, self.lgn_kernel, stride=1) )
-        v1 = self.sigmoid( self.v1(lgn) )
+        if self.activation == None:
+            lgn = F.conv2d(x, self.lgn_kernel, stride=self.lgn_stride)
+            v1 = self.v1(lgn)
+        else:
+            lgn = self.activation( F.conv2d(x, self.lgn_kernel, stride=self.lgn_stride) )
+            v1 = self.activation( self.v1(lgn) )
 
-        return (lgn, v1)
+        if rep == "all":
+            return (lgn, v1)
+        else:
+            return v1
